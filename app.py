8495f17cfd45f168ffacea8df2da8d01dc7ae130
dmRohach <77681047+dmRohach@users.noreply.github.com>
2021-05-27 17:37:02 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
-import argparse
-import json
 from executors import *
 from parsers import RisingErrorArgparse, BaseParser, IperfParser
 
 
-parser = argparse.ArgumentParser()
+parser = RisingErrorArgparse()
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-p', help='keyword password')
 group.add_argument('-f', help='password from file')
 parser.add_argument('host', type=str, help='connection address')
 parser.add_argument('client', type=str, help='client address (whom to listen)')
 
-
-args = parser.parse_args()
-executor = IperfExecutor(args).give_result()
-parser = IperfParser(executor, args)
-parser.print_json()
-#except Exception as e:
-    #parser = BaseParser(None, sys_error=str(e))
-    #parser.print_json()
+try:
+    args = parser.parse_args()
+    executor = IperfExecutor(args).give_result()
+    parser = IperfParser(executor, args)
+    parser.print_json()
+except Exception as e:
+    parser = BaseParser(None, sys_error=str(e))
+    parser.print_json()
