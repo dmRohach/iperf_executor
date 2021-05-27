6950cd5267170c9b447afb77792aeca5280e76af
dmRohach <77681047+dmRohach@users.noreply.github.com>
2021-05-27 16:31:53 +0300

    Add files via upload

--- parsers/iperf_parser.py
+++ parsers/iperf_parser.py
@@ -7,7 +7,7 @@ class IperfParser(BaseParser):
         if not executor_result["error"]:
             self.executor_result_split = executor_result["result"].split('\n')[-5].split()  # Deleting unnecessary information
             self.result = {
-                "IPs": str(args.split("@")[1] + ", " + args.client),
+                "IPs": str(args.host.split("@")[1] + ", " + args.client),
                 "Hostname": str(args.host.split("@")[0]),
                 "Interval": str(self.executor_result_split[2] + " " + self.executor_result_split[3]),
                 "Transfer": str(self.executor_result_split[4] + " " + self.executor_result_split[5]),
